513ab09f12d5d139400a1f8b4edab39a28c09001
a-cute-Alien <954845887@qq.com>
2021-08-03 13:08:40 +0800

    add UserService done

--- src/test/java/JWTTest.java
+++ src/test/java/JWTTest.java
@@ -1,2 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.Map;
+
 public class JWTTest {
+    public static void main(String[] args) {
+        System.out.println("this is addUserService");
+    }
 }
